3a01528be9f76af834480bcbe11a1dd18ccb9d3f
ymr <479634319@qq.com>
2019-10-27 21:41:20 +0800

    更新 ui

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -2,8 +2,13 @@
   'use strict';
 
   $(function() {
+    var $fullText = $('.admin-fullText');
     $('#admin-fullscreen').on('click', function() {
       $.AMUI.fullscreen.toggle();
     });
+
+    $(document).on($.AMUI.fullscreen.raw.fullscreenchange, function() {
+      $.AMUI.fullscreen.isFullscreen ? $fullText.text('关闭全屏') : $fullText.text('开启全屏');
+    });
   });
-})(window.Zepto);
+})(jQuery);
